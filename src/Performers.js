12031d6431a1db669345e959db2182df01e94891
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-26 13:05:34 +0530

    updated

---
+++ src/Performers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Performers = () => {
+  return (
+    <div>
+      <h2>These are the MTC Base Headline Artists... </h2>
+    </div>
+  )
+}
+
+export default Performers;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Artists from './Artists';
+import { Suspense, lazy } from 'react';
 
-class App extends React.Component {
-    render(){
-        return(
-            <div className="App">
-            <Artists />
-            </div>
-        );
-    }
+// import Artists from './Artists';
+
+// class App extends React.Component {
+//     render(){
+//         return(
+//             <div className="App">
+//             <Artists />
+//             </div>
+//         );
+//     }
+// }
+
+const Artists = lazy(() => import('./Artists'))
+const Performers = lazy(() => import('./Performers'))
+
+const App = () => {
+    return (
+        <div className="App">
+            <Suspense fallback={(<h1>Loading...</h1>)}>
+                <Artists />
+            </Suspense>
+            <Suspense fallback={(<h1>Loading...</h1>)}>
+                <Performers />
+            </Suspense>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
